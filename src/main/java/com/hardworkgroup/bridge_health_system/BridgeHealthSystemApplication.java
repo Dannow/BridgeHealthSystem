c5bd183169908b71cb1600ecff6f44181c6543b0
junhui <1393700297@qq.com>
2021-03-03 16:19:57 +0800

    fist commit of permission_management

--- src/main/java/com/hardworkgroup/bridge_health_system/BridgeHealthSystemApplication.java
+++ src/main/java/com/hardworkgroup/bridge_health_system/BridgeHealthSystemApplication.java
@@ -1,14 +1,17 @@
 package com.hardworkgroup.bridge_health_system;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+//1.配置springboot的包扫描
 @SpringBootApplication
+@MapperScan("com.hardworkgroup.bridge_health_system.permission_management.dao")
+
 public class BridgeHealthSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BridgeHealthSystemApplication.class, args);
     }
-
 }
